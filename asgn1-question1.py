405e122ee2f59897b29ee3031f362cdf23ddaa6a
Manju Shettar <manjshettar@gmail.com>
2023-09-28 12:11:34 -0700

    first push

---
+++ asgn1-question1.py
@@ -0,0 +1,3 @@
+print("Full name: Manju Shettar\n")
+print("Pronouns: He/him\n")
+print("My favorite movie is Dune and I'm really excited for part 2 to be adapted. My favorite food is probably Chipotle.\n")
