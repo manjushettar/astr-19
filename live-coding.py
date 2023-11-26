e9a62ff7dd32f9e3bd926e06c50d3cfbb43c4ea6
Manju Shettar <manjshettar@gmail.com>
2023-11-25 21:20:41 -0800

    Commit

---
+++ live-coding.py
@@ -0,0 +1,12 @@
+import sys
+import numpy
+
+def main():
+    x = 0.0
+
+    for i in range(0, 20):
+        x = 2*i + 19.0
+        print(f'The value of x is = {x}')
+
+if __name__ == "__main__":
+    main()
